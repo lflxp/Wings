811994b36b0b9275725eb977928073c70edae26e
lflxp <lxp@lflxp.cn>
2017-12-08 00:58:38 +0800

    function

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 )
 
 func main() {
+	//注册服务
 	err := register.Register("conf/app.conf")
 	if err != nil {
 		fmt.Println("Error",err.Error())
 		return
 	}
 	fmt.Println("ok")
+	//启动监控api
+
+	//启动rpc服务
 
 	// fmt.Println(os.GetHostname())
 	// fmt.Println(runtime.GOARCH)
